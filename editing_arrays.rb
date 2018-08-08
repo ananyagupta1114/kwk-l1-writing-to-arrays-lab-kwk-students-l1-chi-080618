fb0cd1deefce74c960d07886fe180cc5b7488e76
ananyagupta1114 <ananyagupta1114@gmail.com>
2018-08-08 15:54:15 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -12,7 +12,7 @@ def change_rainbow_colors
 
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
 end
-
+puts change_rainbow_colors
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
 end
